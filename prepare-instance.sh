19bf9d113d8d3230467e1b6d1a83f17d8ef4ecc9
Saad Ahmad <ahmadsaad58@gmail.com>
2022-03-29 23:35:57 -0700

    Fixed Prepare Instance (#11)
    * adding sudo
    
    * added -y to docker command

--- prepare-instance.sh
+++ prepare-instance.sh
@@ -4,8 +4,8 @@ echo "Preparing EC2 Ubuntu Instance"
 # update ubuntu
 sudo apt update
 sudo apt full-upgrade -y
-apt-get autoremove -y
-apt-get autoclean -y
+sudo apt-get autoremove -y
+sudo apt-get autoclean -y
 
 echo "***********************"
 echo "Installing make and git"
@@ -28,7 +28,7 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update -y
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io
-sudo apt-get install docker-ce=5:20.10.13~3-0~ubuntu-focal docker-ce-cli=5:20.10.13~3-0~ubuntu-focal containerd.io
+sudo apt-get -y install docker-ce=5:20.10.13~3-0~ubuntu-focal docker-ce-cli=5:20.10.13~3-0~ubuntu-focal containerd.io
 echo "-----------------------"
 sudo docker --version
 echo "-----------------------"
